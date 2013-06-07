fbf048d34c9872d842bfc3696ccbb96b41947ece
Your Real Name <jamiesoung@Gmail.com>
2013-06-07 21:28:13 +0800

    update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div class="container_12 footer">
-        版权所有，保留一切权利！ © 2013 听海阁　<a href="http://www.miibeian.gov.cn/" rel="nofollow"">冀ICP备12006828号</a>
+        版权所有，保留一切权利！ © 2013 <a href="http://www.tinghaige.com/" >听海阁</a>　<a href="http://www.miibeian.gov.cn/" rel="nofollow>冀ICP备12006828号</a>
 <div style="display:none">
 <script src="http://s13.cnzz.com/stat.php?id=4868265&web_id=4868265" language="JavaScript"></script>
 </div>
